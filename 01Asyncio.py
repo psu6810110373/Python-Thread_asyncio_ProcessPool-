c2c7b272887f5df5facde36ae3b212ded0c8f7d3
Sittinon Dormae <6810110373@psu.ac.th>
2026-02-22 17:14:51 +0700

    asyncio_program

---
+++ 01Asyncio.py
@@ -0,0 +1,26 @@
+import asyncio
+import time
+
+async def check_sever(host, port=443):
+    try :
+        reader, writer = await asyncio.wait_for(
+            asyncio.open_connection(host,port), timeout= 3.0
+        ) 
+        writer.close()
+        await writer.wait_closed()
+        return f"{host} ONLINE"
+    except Exception:
+        return f"{host} OFFLINE"
+    
+async def scan_servers(host):
+    task = [check_sever(host) for host in host]
+    return await asyncio.gather(*task)
+
+
+if __name__ == '__main__':
+    hosts_to_check = ["google.com", "github.com", "python.org", "invalid-domain-test.local"]
+    start_time = time.time()
+    results = asyncio.run(scan_servers(hosts_to_check))
+    for res in results :
+        print(res)
+    print(f"เวลาทำงาน: {time.time() - start_time:.2f} วินาที")
